7568c257d4f6c119fdb29a78a15b76e947fb9cb3
Lilian Gallon <lilian.gallon@4sh.fr>
2022-11-18 23:36:01 +0100

    feat(forge): #9 Implemented entrypoint

--- forge/src/main/kotlin/dev/gallon/aimassistance/forge/AimAssistance.kt
+++ forge/src/main/kotlin/dev/gallon/aimassistance/forge/AimAssistance.kt
@@ -1,14 +1,43 @@
 package dev.gallon.aimassistance.forge
 
+import dev.gallon.aimassistance.core.domain.AimAssistanceConfig
+import dev.gallon.aimassistance.core.services.AimAssistanceService
+import dev.gallon.aimassistance.fabric.events.RenderEvent
+import dev.gallon.aimassistance.fabric.events.SingleEventBus
+import dev.gallon.aimassistance.fabric.events.TickEvent
+import dev.gallon.aimassistance.forge.adapters.ForgeMinecraftAdapter
+import dev.gallon.aimassistance.forge.adapters.ForgeMouseAdapter
+import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.Mod
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 
 @Mod("aimassistance")
 class AimAssistance {
-    private val LOGGER: Logger = LogManager.getLogger()
+    private var aimAssistance: AimAssistanceService? = null
 
     init {
-        LOGGER.info("example mod initialization")
+//        AutoConfig.register(ModConfig::class.java, ::JanksonConfigSerializer)
+//        val config = AutoConfig.getConfigHolder(ModConfig::class.java).config
+
+        SingleEventBus.register<TickEvent> {
+//            initOrResetAimAssistance(config.config)
+            aimAssistance?.analyseEnvironment()
+            aimAssistance?.analyseBehavior()
+        }
+
+        SingleEventBus.register<RenderEvent> {
+            aimAssistance?.assistIfPossible()
+        }
+    }
+
+    private fun initOrResetAimAssistance(config: AimAssistanceConfig) {
+        if (aimAssistance == null && Minecraft.getInstance().player != null) {
+            aimAssistance = AimAssistanceService(
+                minecraft = ForgeMinecraftAdapter(Minecraft.getInstance()),
+                mouse = ForgeMouseAdapter(),
+                config = config
+            )
+        } else if (aimAssistance != null && Minecraft.getInstance().player == null) {
+            aimAssistance = null
+        }
     }
 }
