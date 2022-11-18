9a25f0169b5f79ecedf89f065dc0027218d4814c
Lilian Gallon <lilian.gallon@4sh.fr>
2022-11-18 23:35:28 +0100

    feat(forge): #9 Implemented forge adapters

---
+++ forge/src/main/kotlin/dev/gallon/aimassistance/forge/adapters/ForgeMouseAdapter.kt
@@ -0,0 +1,20 @@
+package dev.gallon.aimassistance.forge.adapters
+
+import dev.gallon.aimassistance.core.interfaces.Mouse
+import dev.gallon.aimassistance.fabric.events.LeftMouseClickEvent
+import dev.gallon.aimassistance.fabric.events.MouseMoveEvent
+import dev.gallon.aimassistance.fabric.events.SingleEventBus
+
+class ForgeMouseAdapter : Mouse {
+    private var moved = false
+    private var leftClicked = false
+
+    init {
+        SingleEventBus.register<MouseMoveEvent> { moved = true }
+        SingleEventBus.register<LeftMouseClickEvent> { leftClicked = true }
+    }
+
+    override fun wasLeftClicked(): Boolean = leftClicked.also { leftClicked = false }
+
+    override fun wasMoved(): Boolean = moved.also { moved = false }
+}
